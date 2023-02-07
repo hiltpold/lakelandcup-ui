8d1b289d8c75934257051ef92e1bb882f11b4b36
hiltpold <m.hiltpold@protonmail.ch>
2023-02-07 23:19:41 +0100

    draft prospect

--- src/routes/prospects/index.tsx
+++ src/routes/prospects/index.tsx
@@ -7,39 +7,56 @@ import formReducer, { FormEnum, LeagueTypeForm } from "../../utils/reducers";
 import Grid from "../../components/grid";
 import { UserType, LeagueType } from "../../components/app";
 import { AgGridReact } from "ag-grid-react";
-import { get } from "../../utils/requests";
+import post, { get } from "../../utils/requests";
 import { GridOptions, RowSelectedEvent, ColDef, GridReadyEvent } from "ag-grid-community";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { KeyboardEvent } from "react";
 import { setServers } from "dns";
+import test from "node:test";
+import { stringify } from "querystring";
 
 const draftYear = ["2017", "2018", "2019", "2020", "2021", "2022", "2023"];
 const draftRounds = ["1", "2"];
 const pickInRound = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
 
+type DraftPick = {
+    DraftYear: string;
+    DraftRound: string;
+    DraftPickInRound: string;
+    DraftPickOverall: string;
+};
 type Prospect = {
     ID: string;
     FullName: string;
-    FirstName: string;
-    LastName: string;
     NhlTeam: string;
-    DraftYear: string;
     Birthdate: string;
-    Height: string;
-    Weight: string;
-    NhlDraftRound: string;
-    NhlDraftPickOverall: string;
-    NhlPickInRound: string;
-    PositionCode: string;
     LeagueID: string;
     FranchiseID: string;
-    PickID: string;
+    Pick: {
+        ID: string;
+        DraftYear: string;
+        DraftRound: string;
+        DraftPickInRound: string;
+        DraftPickOverall: string;
+        ProspectID: string;
+    };
+};
+
+type ProspectView = {
+    ID: string;
+    FullName: string;
+    Franchise: string;
+    NhlTeam: string;
+    Birthdate: string;
+    LeagueID: string;
+    FranchiseID: string;
 };
 
 const columnDefsProspect = [
     { field: "FullName" },
+    { field: "Franchise" },
     { field: "NhlTeam" },
     // { field: "NhlDraftRound" },
     // { field: "NhlPickInRound" },
@@ -72,20 +89,14 @@ type Franchise = {
 };
 
 type Draft = {
-    draftYear: string;
-    draftRound: string;
-    draftPickInRound: string;
-    draftPickOverall: string;
+    draftPick: string | DraftPick;
     prospectID: string;
     franchiseID: string;
     leagueID: string;
 };
 
 const initDraftPick = {
-    draftYear: "",
-    draftRound: "",
-    draftPickInRound: "",
-    draftPickOverall: "",
+    draftPick: "",
     prospectID: "",
     franchiseID: "",
     leagueID: "",
@@ -99,20 +110,29 @@ const Prospects: FunctionComponent<{ users: UserType[]; league: LeagueType | und
     const [formData, setFormData] = useReducer(formReducer<Draft>, initDraftPick);
     const [search, setSearch] = useState<string>("");
     const [franchises, setFranchises] = useState<Franchise[]>([]);
-    const [prospectId, setProspectId] = useState<string>("");
+    const [draftPicks, setDraftPicks] = useState<DraftPick[]>([]);
+
     const [gridOptions, setGridOptions] = useState<GridOptions>(gridOptionsPropspectInit);
 
     const onSelectionChanged = (params: any) => {
         const selectedRow = params.api.getSelectedRows()[0];
-        console.log(selectedRow);
-        setFormData({
-            type: FormEnum.Set,
-            payload: {
-                name: "prospectID",
-                value: selectedRow.ID,
-            },
-        });
-        setProspectId(selectedRow.ID);
+        if (selectedRow) {
+            setFormData({
+                type: FormEnum.Set,
+                payload: {
+                    name: "prospectID",
+                    value: selectedRow.ID,
+                },
+            });
+        } else {
+            setFormData({
+                type: FormEnum.Set,
+                payload: {
+                    name: "prospectID",
+                    value: "",
+                },
+            });
+        }
     };
 
     const handleChange = ({
@@ -126,14 +146,31 @@ const Prospects: FunctionComponent<{ users: UserType[]; league: LeagueType | und
                     value: currentTarget.value,
                 },
             });
+        } else if (currentTarget.name == "draftYear") {
+            const draftPicksPerRound = draftRounds.flatMap((dr) => {
+                const picks: DraftPick[] = pickInRound.map((pid) => {
+                    return {
+                        DraftYear: currentTarget.value,
+                        DraftRound: dr,
+                        DraftPickInRound: pid,
+                        DraftPickOverall: (
+                            (parseInt(dr) - 1) * pickInRound.length +
+                            parseInt(pid)
+                        ).toString(),
+                    };
+                });
+                return picks;
+            });
+            setDraftPicks(draftPicksPerRound);
+        } else {
+            setFormData({
+                type: FormEnum.Set,
+                payload: {
+                    name: currentTarget.name,
+                    value: currentTarget.value,
+                },
+            });
         }
-        setFormData({
-            type: FormEnum.Set,
-            payload: {
-                name: currentTarget.name,
-                value: currentTarget.value,
-            },
-        });
     };
 
     const handleDraft = (event: JSX.TargetedEvent<HTMLFormElement, Event>) => {
@@ -148,38 +185,23 @@ const Prospects: FunctionComponent<{ users: UserType[]; league: LeagueType | und
             formData.prospectID !== undefined &&
             formData.prospectID !== null &&
             formData.prospectID !== "" &&
-            formData.draftYear !== undefined &&
-            formData.draftYear !== null &&
-            formData.draftYear !== "" &&
-            formData.draftRound !== undefined &&
-            formData.draftRound !== null &&
-            formData.draftRound !== "" &&
-            formData.draftPickInRound !== undefined &&
-            formData.draftPickInRound !== null &&
-            formData.draftPickInRound !== ""
+            formData.draftPick !== undefined &&
+            formData.draftPick !== null &&
+            formData.draftPick !== ""
         ) {
-            formData.draftPickOverall =
-                formData.draftRound === "1"
-                    ? formData.draftPickInRound
-                    : (
-                          (parseInt(formData.draftRound) - 1) * pickInRound.length +
-                          parseInt(formData.draftPickInRound)
-                      ).toString();
-            /*
-            setFormData({
-                type: FormEnum.Set,
-                payload: {
-                    name: "draftPickOverall",
-                    value:
-                        formData.draftRound === "1"
-                            ? formData.draftPickInRound
-                            : (
-                                  (parseInt(formData.draftRound) - 1) * pickInRound.length +
-                                  parseInt(formData.draftPickInRound)
-                              ).toString(),
-                },
+            if (typeof formData.draftPick === "string") {
+                formData.draftPick = JSON.parse(formData.draftPick);
+            }
+            // post data
+            post(`${process.env.BASE_URL_FANTASY_SVC}/prospect/draft`, formData).then((data) => {
+                if (data.status == 201) {
+                    console.log(`API response code ${data.status}`);
+                } else {
+                    // TODO: handle error api response
+                    console.log(`API response ${data}`);
+                    console.log(data);
+                }
             });
-            */
             console.log(formData);
         }
     };
@@ -187,30 +209,54 @@ const Prospects: FunctionComponent<{ users: UserType[]; league: LeagueType | und
     const searchProspects = (e: JSX.TargetedKeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter" && search.length > 0) {
             setSearch(e.currentTarget.value);
-            get(`${process.env.BASE_URL_FANTASY_SVC}/prospects/${encodeURIComponent(search)}`)
+            get(`${process.env.BASE_URL_FANTASY_SVC}/prospect/${encodeURIComponent(search)}`)
                 .then((data) => {
                     if (data.status != 200) {
                         // TODO: handle error api response
                         console.log(`API response code ${data.status}`);
                     } else {
                         if (data.prospects && data.prospects.length >= 0) {
-                            const rowData = data.prospects.map((p: Prospect) => {
+                            console.log(data.prospects);
+                            const prospects = data.prospects.map((p: Prospect) => {
                                 return {
+                                    ID: p.ID,
                                     FullName: p.FullName,
                                     NhlTeam: p.NhlTeam,
-                                    NhlDraftRound: p.NhlDraftRound,
-                                    NhlPickInRound: p.NhlPickInRound,
-                                    NhlDraftPickOverall: p.NhlDraftPickOverall,
                                     Birthdate: p.Birthdate,
-                                    PositionCode: p.PositionCode,
-                                    Weight: p.Weight,
-                                    Height: p.Height,
-                                    ID: p.ID,
                                     FranchiseID: p.FranchiseID,
                                     LeagueID: p.LeagueID,
+                                    Pick: {
+                                        ID: p.Pick.ID,
+                                        DraftYear: p.Pick.DraftYear,
+                                        DraftRound: p.Pick.DraftRound,
+                                        DraftPickInRound: p.Pick.DraftPickInRound,
+                                        DraftPickOverall: p.Pick.DraftPickOverall,
+                                        ProspectID: p.Pick.ProspectID,
+                                    },
                                 };
                             });
-                            setGridOptions({ ...gridOptions, rowData: rowData });
+                            const prospectsView = prospects.map(async (p: Prospect) => {
+                                let tmp: ProspectView = {
+                                    FullName: p.FullName,
+                                    Franchise: "",
+                                    Birthdate: p.Birthdate,
+                                    NhlTeam: p.NhlTeam,
+                                    ID: p.ID,
+                                    LeagueID: p.LeagueID,
+                                    FranchiseID: p.FranchiseID,
+                                };
+                                const fID = p.FranchiseID;
+                                if (fID !== undefined && fID !== null && fID !== "") {
+                                    const franchise: Franchise = await get(
+                                        `${process.env.BASE_URL_FANTASY_SVC}/franchise/${fID}}`,
+                                    );
+                                    tmp.Franchise = franchise.Name;
+                                }
+                                return tmp;
+                            });
+                            Promise.all(prospectsView).then((pv) => {
+                                setGridOptions({ ...gridOptions, rowData: pv });
+                            });
                         } else {
                             setGridOptions({ ...gridOptions, rowData: [] });
                         }
@@ -281,28 +327,19 @@ const Prospects: FunctionComponent<{ users: UserType[]; league: LeagueType | und
                                 <option value={f.ID}>{f.Name}</option>
                             ))}
                         </select>
-                        <label className={`form-label ${style.label}`}>Draft Round</label>
+                        <label className={`form-label ${style.label}`}>Draft Pick</label>
                         <select
                             class="form-select"
-                            name="draftRound"
+                            name="draftPick"
                             type="text"
                             onChange={handleChange}
+                            disabled={false}
                         >
                             <option value="">{""}</option>
-                            {draftRounds.map((dr) => (
-                                <option value={dr}>{dr}</option>
-                            ))}
-                        </select>
-                        <label className={`form-label ${style.label}`}>Pick in Round</label>
-                        <select
-                            class="form-select"
-                            name="draftPickInRound"
-                            type="text"
-                            onChange={handleChange}
-                        >
-                            <option value="">{""}</option>
-                            {pickInRound.map((pir) => (
-                                <option value={pir}>{pir}</option>
+                            {draftPicks.map((dr) => (
+                                <option
+                                    value={JSON.stringify(dr)}
+                                >{`Round: ${dr.DraftRound}  |  Pick ${dr.DraftPickInRound}  |  Pick Overall: ${dr.DraftPickOverall}`}</option>
                             ))}
                         </select>
                     </div>
